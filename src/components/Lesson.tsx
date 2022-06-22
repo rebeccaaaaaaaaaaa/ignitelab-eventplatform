444902276fc1347d1a277eec1ea1ccba7856e264
rebeccaaaaaaaaaaa <rebecca.ignacio@orramtech.com.br>
2022-06-22 00:50:53 -0300

    feat: add circle icon

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CheckCircle } from "phosphor-react"
 
 function Lesson() {
   return (
@@ -8,7 +9,8 @@ function Lesson() {
       </span>
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          <span className="text-sm text-blue-500 font-medium"> 
+          <span className="text-sm text-blue-500 font-medium flex items-center gap-2"> 
+            <CheckCircle size={20} />
             Conteúdo liberado
           </span>
           <span className="text-sx rounded px-2 py-[2px] text-white border border-green-300 font-bold"> 
