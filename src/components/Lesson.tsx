4f758c59ec44e0857a443d1bd2912aa66151c560
rebeccaaaaaaaaaaa <rebecca.ignacio@orramtech.com.br>
2022-06-22 01:57:33 -0300

    feat: configure for use date fns lib-

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { CheckCircle, Lock } from "phosphor-react"
+import { isPast, format } from "date-fns"
+import ptBR from 'date-fns/locale/pt-BR';
 
 interface LessonProps {
   title: string,
@@ -11,11 +13,16 @@ interface LessonProps {
 
 export function Lesson(props: LessonProps) {
 
-  const isLessonAvailable = false;
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+    locale: ptBR,
+  })
 
   return (
     <a href="">
-      <span className="text-gray-300">{props.availableAt.toString()}</span>
+      <span className="text-gray-300 capitalize">
+        {availableDateFormated}
+      </span>
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
