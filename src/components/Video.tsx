bd8a2b7713190db8aa931e805c6b6cba42265367
rebeccaaaaaaaaaaa <rebecca.ignacio@orramtech.com.br>
2022-06-23 05:58:03 -0300

    feat: create a content struture and styles

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Image,
+  Lightning,
+} from "phosphor-react";
+import React from "react";
 
 export function Video() {
   return (
     <div className="flex-1">
-      Video
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w=[1100px] max-h-[60vh] aspect-video  "></div>
+      </div>
+
+      <div className="flex flex-col p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl bold"> Aula 1 - Abertura do Ignite Lab</h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              Nessa aula vamos dar início ao projeto criando a estrutura base da
+              aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também
+              realizar o setup do nosso projeto no GraphCMS criando as entidades
+              da aplicação e integrando a API GraphQL gerada pela plataforma no
+              nosso front-end utilizando Apollo Client.
+            </p>
+
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                src="https://avatars.githubusercontent.com/u/71036610?v=4"
+                alt=""
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+              />
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">
+                  {" "}
+                  Rebecca Ignacio
+                </strong>
+                <span className="text-gray-200 text-sm block">
+                  {" "}
+                  Front-end Developer
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <a
+              href=""
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Comunidade do discord
+            </a>
+            <a
+              href=""
+              className="p-4 text-sm text-blue-500 border border-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
+              <Lightning size={24} />
+              Desafios
+            </a>
+          </div>
+        </div>
+
+        <div className="gap-8 mt-20 grid grid-cols-2">
+        <a
+          href=""
+          className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6  hover:bg-gray-600 transition-colors"
+        >
+          <div className="bg-green-700 h-full p-6 flex items-center">
+            <FileArrowDown size={40} />
+          </div>
+          <div className="py-6 leading-relaxed">
+            <strong className="text-2xl"> Material complementar </strong>
+            <p className="text-sm text-gray-200">
+              Acesse o material complementar para acelerar o seu desenvolvimento
+            </p>
+          </div>
+          <div className="h-full p-6 flex items-center">
+            <CaretRight size={24} />
+          </div>
+        </a>
+
+        <a
+          href=""
+          className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6  hover:bg-gray-600 transition-colors"
+        >
+          <div className="bg-green-700 h-full p-6 flex items-center">
+            <Image size={40} />
+          </div>
+          <div className="py-6 leading-relaxed">
+            <strong className="text-2xl"> Wallpapers exclusivos </strong>
+            <p className="text-sm text-gray-200">
+              Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+            </p>
+          </div>
+          <div className="h-full p-6 flex items-center">
+            <CaretRight size={24} />
+          </div>
+        </a>
+      </div>
+      </div>
+
+     
     </div>
-  )
+  );
 }
