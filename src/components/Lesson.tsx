315e8f8fa79b28ee4ec045c36e5b0ca4bb8b7f5b
rebeccaaaaaaaaaaa <rebecca.ignacio@orramtech.com.br>
2022-06-24 01:47:57 -0300

    feat: ending subscribe page and api integration

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CheckCircle, Lock } from "phosphor-react"
 import { isPast, format } from "date-fns"
 import ptBR from 'date-fns/locale/pt-BR';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 interface LessonProps {
   title: string,
@@ -14,20 +14,31 @@ interface LessonProps {
 
 export function Lesson(props: LessonProps) {
 
+  const {slug} = useParams<{slug: string}>()
+
   const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR,
   })
+
+  const isActiveLesson = slug === props.slug;
+
 
   return (
     <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300 capitalize">
         {availableDateFormated}
       </span>
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <div className={
+        `rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' : ''}`
+      }>
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <span className={
+              `text-sm text-blue-500 font-medium flex items-center gap-2 
+                ${isActiveLesson ? 'text-white' : ''}
+              `
+            }>
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
@@ -37,11 +48,20 @@ export function Lesson(props: LessonProps) {
               Em breve
             </span>
           )}
-          <span className="text-sx rounded px-2 py-[2px] text-white border border-green-300 font-bold">
+          <span className={
+            `text-sx rounded px-2 py-[2px] text-white border font-bold
+             ${isActiveLesson ? 'border-white' : 'border-green-300'}
+            
+            `
+          }>
             {props.type === "live" ? "Ao vivo" : "Aula prática"}
           </span>
         </header>
-        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+        <strong className={
+          `mt-5 block
+            ${isActiveLesson ? 'text-white' : 'text-gray-200'}
+          `
+        }>{props.title}</strong>
       </div>
     </Link>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import { Footer } from "../components/Footer";
 import CodeImg from "../assets/home_img.png";
 import { Logo } from "../components/Logo";
+import { gql, useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+
+const CREATE_SUBSCRIBE_MUTATION = gql`
+  mutation CreateSubscriber($name: String!, $email: String!) {
+    createSubscriber(data: { name: $name, email: $email }) {
+      id
+    }
+  }
+`;
 
 export function Home() {
+
+ const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBE_MUTATION);
+
+  async function handleSubscribe(e: FormEvent) {
+    e.preventDefault();
+
+   await createSubscriber({
+      variables: {
+        name,
+        email,
+      },
+    });
+
+    navigate('/event')
+  }
+
   return (
     <div className="bg-blur bg-cover bg-no-repeat">
       <div className="flex items-center justify-center gap-48 pt-20">
@@ -11,8 +42,10 @@ export function Home() {
           <Logo />
           <h1 className="text-4xl mt-4 leading-tight">
             Construa uma
-            <span className="text-blue-500"> aplicação completa</span> <br /> do
-            zero com
+            <span className="text-blue-500">
+              {" "}
+              aplicação completa
+            </span> <br /> do zero com
             <span className="text-blue-500"> React JS</span>
           </h1>
           <p className="mt-5 text-gray-200 leading-relaxed">
@@ -23,12 +56,32 @@ export function Home() {
         </div>
 
         <div className="p-8 bg-gray-600 border-gray-500 rounded">
-          <strong className="text-2xl block mb-6 ">Inscreva-se gratuitamente</strong>
-          <form className="flex flex-col gap-3 w-full">
-            <input placeholder="Seu nome completo" type="text" className="bg-gray-900 rounded px-5 h-14"/>
-            <input placeholder="Seu e-mail" type="email" className="bg-gray-900 rounded px-5 h-14"/>
+          <strong className="text-2xl block mb-6 ">
+            Inscreva-se gratuitamente
+          </strong>
+          <form
+            className="flex flex-col gap-3 w-full"
+            onSubmit={handleSubscribe}
+          >
+            <input
+              placeholder="Seu nome completo"
+              type="text"
+              className="bg-gray-900 rounded px-5 h-14"
+              onChange={(event) => setName(event.target.value)}
+            />
 
-            <button className="block bg-green-500 mt-4 uppercase py-4 font-bold text-sm hover:bg-green-300 transition-colors"> Garantir minha vaga</button>
+            <input
+              placeholder="Seu e-mail"
+              type="email"
+              className="bg-gray-900 rounded px-5 h-14"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+
+            <button 
+                disabled={loading}
+                className="block bg-green-500 mt-4 uppercase py-4 font-bold text-sm hover:bg-green-300 transition-colors disabled:opacity-50">
+              Garantir minha vaga
+            </button>
           </form>
         </div>
       </div>
